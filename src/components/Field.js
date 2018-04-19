43bda3d6aeb59e64afbeb7e992dcf646a0a4bc22
Steve Godin <steve.god@gmail.com>
2018-04-19 15:27:17 -0700

    Add demo for Styling Fields & Main

--- src/components/Field.js
+++ src/components/Field.js
@@ -3,7 +3,12 @@ import React from "react";
 function Field(props) {
   return (
     <p className="Field">
-      <strong>{props.name}:</strong> <em>{props.value}</em>
+      <strong style={{
+        color: "white",
+        backgroundColor: "maroon",
+        borderRadius: "3px",
+        padding: "0 4px"
+      }}>{props.name}</strong> <em>{props.value}</em>
     </p>
   )
 };
