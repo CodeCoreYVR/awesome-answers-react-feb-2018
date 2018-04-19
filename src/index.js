fff67ca18c46f0e670097b291d78f50eba4a60d4
Steve Godin <steve.god@gmail.com>
2018-04-19 14:07:41 -0700

    Add demo for Question Details

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+function QuestionDetails(props) {
+  return (
+    <div className="QuestionDetails">
+      <h1>What is your favourite color?</h1>
+      <p>Red, green, blue, magenta, yellow, cyan, etc.</p>
+      <p>By Bridge Troll</p>
+      <p><strong>View Count:</strong> 111</p>
+      <p>Created 10 days ago</p>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
+  <QuestionDetails />,
   document.getElementById('root')
 );
 registerServiceWorker();
+
+
+
+
+//
