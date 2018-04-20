8e4691bed9571fc6c0cd75f20fb0672c92be9deb
Steve Godin <steve.god@gmail.com>
2018-04-20 13:24:24 -0700

    Add exercise for Question Index to Class

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import { Field } from "./Field";
 import allQuestions from "../data/allQuestions";
 
-function QuestionIndexPage(props) {
-  return (
-    <main className="QuestionIndexPage">
-      <h1>Questions</h1>
-      <ul>
-        {
-          allQuestions.map(
-            (question, index) => (
-              <li key={question.id}>
-                <a href="">{question.title}</a>
-                <Field name="Author" value={question.author.full_name} />
-              </li>
+class QuestionIndexPage extends Component {
+  render () {
+    return (
+      <main className="QuestionIndexPage">
+        <h1>Questions</h1>
+        <ul>
+          {
+            allQuestions.map(
+              (question, index) => (
+                <li key={question.id}>
+                  <a href="">{question.title}</a>
+                  <Field name="Author" value={question.author.full_name} />
+                </li>
+              )
             )
-          )
-        }
-      </ul>
-    </main>
-  )
+          }
+        </ul>
+      </main>
+    )
+  }
 }
 
 export { QuestionIndexPage };
