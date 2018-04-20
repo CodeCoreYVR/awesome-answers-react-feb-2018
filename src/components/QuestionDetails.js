ec23ef019da2e6210abc7b4f4a11fa34c149ccff
Steve Godin <steve.god@gmail.com>
2018-04-20 13:19:42 -0700

    Add demo for Question Show to Class

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -6,7 +6,7 @@ function QuestionDetails(props) {
     <div className="QuestionDetails">
       <h1>{props.title}</h1>
       <p>{props.body}</p>
-      <p>By {props.user.full_name}</p>
+      <p>By {props.author.full_name}</p>
       <Field name="View Count" value={props.view_count} />
       <Field name="Created At" value={props.created_at} />
     </div>
--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,23 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import { QuestionDetails } from "./QuestionDetails";
-import { AnswerDetails } from "./AnswerDetails";
 import { AnswerList } from "./AnswerList";
 import questionDetails from "../data/detailedQuestion";
 
-function QuestionShowPage(props) {
-  return (
-    <main className="QuestionShowPage">
-      <QuestionDetails
-        title="What is your favourite color?"
-        body="Red, Blue, Magenta, etc."
-        user={{full_name: "Steve"}}
-        view_count={1000}
-        created_at={new Date().toLocaleString()}
-      />
-      <h2>Answers</h2>
-      <AnswerList answers={questionDetails.answers} />
-    </main>
-  )
+class QuestionShowPage extends Component {
+  render() {
+    return (
+      <main className="QuestionShowPage">
+        <QuestionDetails {...questionDetails} />
+        <h2>Answers</h2>
+        <AnswerList answers={questionDetails.answers} />
+      </main>
+    )
+  }
 }
 
 export { QuestionShowPage };
